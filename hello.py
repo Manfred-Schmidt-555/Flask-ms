f0d374bb4b24c85917771656978ead117e6f0420
Manfred Schmidt <manfred.schmidt@posteo.de>
2021-05-01 15:16:29 +0200

    Ordner templates und darin Datei index.html eingeführt

--- hello.py
+++ hello.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 # create a rout decorator
 @app.route('/')
 
+#def index():
+#	return "<h1> HelloWorld! </h1>"
+
 def index():
-	return "<h1> HelloWorld! </h1>"
+	return render_template("index.html")
 
 # localhost:5000/user/Manfred
 @app.route('/user/<name>')
 
 def user(name):
-	return	"<h1>Hello {}!</h1>".format(name) 
+	return	"<h1>Hello {}!!!</h1>".format(name) 
