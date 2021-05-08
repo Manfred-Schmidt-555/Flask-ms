6821d46debaf1824387a5f286c9d407641545bba
Manfred Schmidt <manfred.schmidt@posteo.de>
2021-05-08 14:28:59 +0200

    Übergeben eine Liste

--- hello.py
+++ hello.py
@@ -11,11 +11,22 @@ app = Flask(__name__)
 
 def index(): 
 	first_name = "Manfred"
-	stuff = "This is<p> Bold </p>"
-	return render_template("index.html", first_name=first_name)
+	stuff = "This is<strong> Bold </strong>"
+
+	# "list"  einführen
+	favorite_pizza = ["pepperoni", "tomato", "mushroom", 42]
+
+
+	# so, hier verweise ich auf index.html in templates
+	# und kann Werte zuweisen "first_namw, stuff"
+	# in der *.html Daei kann ich "Filter" setzen: "capitalize, lower,…""
+	return render_template("index.html",  
+		first_name=first_name,
+		stuff=stuff, 
+		favorite_pizza = favorite_pizza,) #dies wird in der index.html verwendet
 
 # localhost:5000/user/Manfred
 @app.route('/user/<name>')
 
 def user(name):
-	return render_template("user.html", user_name=name)  
+	return render_template("user.html", user_name=name)
