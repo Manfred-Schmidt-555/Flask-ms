dfeeb9c5dbcd776fabae9e16132f388482b3f229
Manfred Schmidt <manfred.schmidt@posteo.de>
2021-05-09 17:01:18 +0200

    base.html eingeführt

--- hello.py
+++ hello.py
@@ -30,3 +30,15 @@ def index():
 
 def user(name):
 	return render_template("user.html", user_name=name)
+
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)  #eine Flaks Funktion
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+	# Internal Server Error
+@app.errorhandler(500)  #eine Flaks Funktion
+def page_not_found(e):
+	return render_template("500.html"), 500
