cae9fd8606b76a7cca52c4ad76d5f446e47134d6
Manfred Schmidt <manfred.schmidt@posteo.de>
2021-05-14 10:08:56 +0200

    eingeführt wtf (what the form) und messages mit flash

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mein super geheimes Passwort"
+
+# create a Form Class
+class NamerForm(FlaskForm):
+	name =StringField("Whats your name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+ 
 
 # create a rout decorator
 @app.route('/')
@@ -12,6 +24,8 @@ app = Flask(__name__)
 def index(): 
 	first_name = "Manfred"
 	stuff = "This is<strong> Bold </strong>"
+	# ich könnte nun eine flash-meldung hier einfügen
+	flash("Hallo auf dieser Webseite")
 
 	# "list"  einführen
 	favorite_pizza = ["pepperoni", "tomato", "mushroom", 42]
@@ -42,3 +56,17 @@ def page_not_found(e):
 @app.errorhandler(500)  #eine Flaks Funktion
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	# Validate Form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		flash("Form Submitted Successfully")
+	return render_template("name.html",
+		name = name,
+		form = form)
